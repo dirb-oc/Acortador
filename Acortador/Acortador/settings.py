4d3df1777335577a88652c3030205cc740ae5ce5
Orlando Contreras <dirbdeving@gmail.com>
2024-09-26 16:49:26 -0500

    Creacion Proyecto Django Simplificacion

--- Acortador/Acortador/settings.py
+++ Acortador/Acortador/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-veh31bat+frw-3zc)licdfi=ck5fhq4cu08yg^jq$z0isi3)a^'
+SECRET_KEY = 'django-insecure-w)$k(^lmcb1xzn##r5!56*)06f602agvx^ry1bj87pqjmvgu++'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
