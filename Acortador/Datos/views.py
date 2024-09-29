ca3b8537d6b3c6ef1bd82c8ea4853d4907345d4d
Orlando Contreras <dirbdeving@gmail.com>
2024-09-28 23:28:47 -0500

    Version QR

--- Acortador/Datos/views.py
+++ Acortador/Datos/views.py
@@ -1,22 +1,25 @@
-from django.shortcuts import render
-import pyshorteners
 import qrcode
+import base64
+from io import BytesIO
+from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    Link = None
-    resultado = None 
-    
+    qr_image_url = None
+
     if request.method == "POST":
-        # Obtiene los datos del formulario
-        Link = request.POST.get('Link')
-        art = pyshorteners.Shortener()
-        resultado = (art.tinyurl.short(Link))
+        link = request.POST.get('Link')
+        
+        # Generar el código QR
+        img = qrcode.make(link)
+        buffer = BytesIO()
+        img.save(buffer, format="PNG")
+        buffer.seek(0)
 
+        # Codificar la imagen a base64
+        img_base64 = base64.b64encode(buffer.getvalue()).decode('utf-8')
+        qr_image_url = f"data:image/png;base64,{img_base64}"
 
-    return render(request, 'Index.html', {
-        'resultado': resultado
+    return render(request, 'index.html', {
+        'qr_image_url': qr_image_url,
     })
-
-#img = qrcode.make(Acortado)
-#type(img)
-##img.save("QR.png")
